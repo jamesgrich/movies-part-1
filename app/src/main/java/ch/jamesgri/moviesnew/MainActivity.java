19f7c03713d091e006bc7d68b003e6d86d812d3f
jamesgrich <jamesrichardson15@gmail.com>
2018-04-15 18:17:40 +0100

    Main screen now pulling in images sucessfully

--- app/src/main/java/ch/jamesgri/moviesnew/MainActivity.java
+++ app/src/main/java/ch/jamesgri/moviesnew/MainActivity.java
@@ -92,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("MainActivity", "Number of movies received: " + mListofMovies.size());
                     mAdapter.updateMovies(mListofMovies);
                     mAdapter.notifyDataSetChanged();
+
                 } else {
                     int statusCode = response.code();
                     // handle request errors depending on status code
--- app/src/main/java/ch/jamesgri/moviesnew/data/remote/ApiUtils.java
+++ app/src/main/java/ch/jamesgri/moviesnew/data/remote/ApiUtils.java
@@ -6,7 +6,7 @@ package ch.jamesgri.moviesnew.data.remote;
 
 public class ApiUtils {
 
-    public static final String BASE_URL = "http://image.tmdb.org/t/p/";
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
 
     public static final String SIZE = "w185/";
 
